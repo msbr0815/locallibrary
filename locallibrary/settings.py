8fff4e401b141db3f0e587de5f407b53b864bd42
Bräuninger Matthias <braeunin@intranet.epfl.ch>
2020-04-02 18:37:59 +0200

    Created database and models

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('../.authfile','r').readlines()[0].strip() or 'you-will-never-guess'
+SECRET_KEY = open(os.path.join(BASE_DIR, '.authfile'),'r').readlines()[0].strip() or 'you-will-never-guess'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
